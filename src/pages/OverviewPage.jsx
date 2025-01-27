5f755096e18a2335b8fe85988309d2c7b51cbcd1
diegodevtech <diegodevtechnology@gmail.com>
2025-01-27 17:24:37 -0400

    feat: finishes up header component and starts using it in Overview page

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,7 +1,9 @@
+import Header from "../components/common/Header"
+
 const OverviewPage = () => {
   return (
     <div className="flex-1 overflow-auto relative z-10">
-
+      <Header title="Overview"/>
     </div>
   )
 }
