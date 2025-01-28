641c9ae408c1309fc799e5909df7babbd1b80642
diegodevtech <diegodevtechnology@gmail.com>
2025-01-28 13:09:09 -0400

    feat: starts Products page development

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,25 @@
+import Header from "../components/common/Header"
+import { motion } from "framer-motion"
+import StatCard from "../components/common/StatCard"
+import { AlertTriangle, DollarSign, Package, TrendingUp } from "lucide-react"
+
 const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Products"/>
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        <motion.div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCard name="Total Products" icon={Package} value="$1234" color="#6366f1"/>
+          <StatCard name="Top selling" icon={TrendingUp} value="89" color="#10b981"/>
+          <StatCard name="Low Stock" icon={AlertTriangle} value="23" color="#f59e0b"/>
+          <StatCard name="Total Revenue" icon={DollarSign} value="$543.210,00" color="#ef4444"/>
+        </motion.div>
+      </main>
+    </div>
   )
 }
 
