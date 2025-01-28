6840ee85fb1568c4262008c950891be993b584b3
diegodevtech <diegodevtechnology@gmail.com>
2025-01-28 17:13:03 -0400

    feat: finishes up Orders Table

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -3,15 +3,16 @@ import { motion } from "framer-motion";
 
 import Header from "../components/common/Header";
 import StatCard from "../components/common/StatCard";
-import DailyOrders from "../components/orders/DailyOrders";
-import OrderDistribution from "../components/orders/OrderDistribution";
+import DailyOrders from "../components/orders/DailyOrdersChart";
+import OrderDistribution from "../components/orders/OrdersDistributionChart";
 import OrdersTable from "../components/orders/OrdersTable";
 
+
 const orderStats = {
-	totalOrders: "1,234",
+	totalOrders: "1.234",
 	pendingOrders: "56",
-	completedOrders: "1,178",
-	totalRevenue: "$98,765",
+	completedOrders: "1.178",
+	totalRevenue: "$98.765",
 };
 
 const OrdersPage = () => {
