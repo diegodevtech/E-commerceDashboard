995c81acf22ee06b9dd4b218230c6eabc4c3c608
diegodevtech <diegodevtechnology@gmail.com>
2025-01-28 17:18:34 -0400

    feat: adds Overview Card component

--- src/pages/AnalyticsPage.jsx
+++ src/pages/AnalyticsPage.jsx
@@ -6,7 +6,7 @@ const AnalyticsPage = () => {
 			<Header title={"Analytics Dashboard"} />
 
 			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
-				<OverviewCards />
+				<OverviewCard />
 				<RevenueChart />
 
 				<div className='grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8'>
